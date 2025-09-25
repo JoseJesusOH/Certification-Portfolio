70185d8e901d6dd113b38bc657469ffdcc3d50fe
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-25 16:28:13 -0700

    Variables Reglas - JSU
    Codigo para poner en practica las reglas de  JS en cuestion de variables.

---
+++ JavaScript-Universidad/CodigoJS-Curso-P1/variables-reglas.js
@@ -0,0 +1,17 @@
+// letras, $, _ , numeros
+
+let $miNumero1=10;
+console.log($miNumero1)
+
+let _miNumero2=2;
+console.log(_miNumero2)
+
+let miNumero3=21
+console.log(miNumero3)
+
+//Comentarios , suma de 1 +1 
+console.log(1+1)
+
+/*
+  Comentario en linea
+ */
