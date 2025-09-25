4eb26352c30659039d1c7e64db149c51e043d1bd
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-25 16:35:51 -0700

    Tipos Datos JS - JSU
    Implementación de los tipos datos en JS.

---
+++ JavaScript-Universidad/CodigoJS-Curso-P1/tipos-datos.js
@@ -0,0 +1,12 @@
+let miNumero=10;
+console.log(miNumero)
+let miFlotante=7-5;
+console.log(miFlotante)
+let miCadena="Hola";
+console.log(miCadena)
+let miBooleano=true;
+console.log(miBooleano)
+let miNull=null;
+console.log(miNull)
+let miUndefined=undefined;
+console.log(miNull)
