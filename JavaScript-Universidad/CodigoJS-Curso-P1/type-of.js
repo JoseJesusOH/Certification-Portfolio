c85f07bffd56097fcf014f04e5caad77b5608026
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-25 16:39:19 -0700

    Type Of JS - JSU
    Se implementa operador TypeOf JS, para ver el tipo de dato.

--- JavaScript-Universidad/CodigoJS-Curso-P1/type-of.js
+++ JavaScript-Universidad/CodigoJS-Curso-P1/type-of.js
@@ -1,12 +1,18 @@
 let miNumero=10;
 console.log(miNumero)
+console.log(typeof miNumero)
 let miFlotante=7-5;
 console.log(miFlotante)
+console.log(typeof miFlotante)
 let miCadena="Hola";
 console.log(miCadena)
+console.log(typeof miCadena)
 let miBooleano=true;
 console.log(miBooleano)
+console.log(typeof miBooleano)
 let miNull=null;
 console.log(miNull)
+console.log(typeof miNull)
 let miUndefined=undefined;
 console.log(miUndefined)
+console.log(typeof miUndefined)
