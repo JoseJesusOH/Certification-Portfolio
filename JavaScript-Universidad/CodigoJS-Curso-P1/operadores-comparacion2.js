62210f4c8eceaa66723b193a7e7c2e0615e3c1e7
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-26 00:29:18 -0700

    Operadores de Comparación 2 - CEP JSU
    Se realiza un ejemplo basico de los operadores de comparación mayor que, menor que ,etc.

---
+++ JavaScript-Universidad/CodigoJS-Curso-P1/operadores-comparacion2.js
@@ -0,0 +1,16 @@
+let a=5;
+let b='5';
+console.log(a)
+console.log(b)
+
+console.log("a != b  -> ",a!=b)
+//Interpolacion String
+console.log(`${a}!==${b} ->  ${a!==b}`)
+
+console.log("a > b  -> ",a>b)
+//Interpolacion String
+console.log(`${a}>=${b} ->  ${a>=b}`)
+
+console.log("a < b  -> ",a<b)
+//Interpolacion String
+console.log(`${a}<=${b} ->  ${a<=b}`)
