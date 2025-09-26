5befb9999815be91010488b771c390258a532cc2
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-25 17:05:28 -0700

    Operadores JS - JSU
    Se realiza la iomplemtación basioca de los operadores en JS.

---
+++ JavaScript-Universidad/CodigoJS-Curso-P1/operadores.js
@@ -0,0 +1,25 @@
+let a,b,c,d,e,f;
+
+a=3+1;
+console.log(a)
+
+b=6-2;
+console.log(b)
+
+c=a*2;
+console.log(c)
+
+
+d=b/2.5
+console.log(d)
+console.log(typeof d)
+
+f=9%2
+console.log(f)
+
+
+e=2**3
+console.log(e)
+
+e=Math.pow(2,3)
+console.log(e)
