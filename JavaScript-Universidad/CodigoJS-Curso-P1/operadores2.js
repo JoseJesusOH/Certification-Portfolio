4356d4024bcd0d9c6c59205fec8c719050d37d98
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-25 17:10:43 -0700

    Operadores Incremento Decremento - JSU
    Se realiza implementación de los operadores JS para incremento y decremento de datos.

---
+++ JavaScript-Universidad/CodigoJS-Curso-P1/operadores2.js
@@ -0,0 +1,18 @@
+let a,b,c;
+//Preincremento
+a=0;
+++a;
+console.log(a)
+//PostIncremento
+a++;
+console.log(a)
+--a;
+console.log(a)
+a--;
+console.log(a)
+
+a=5;
+b=2;
+c=++a*b--;
+console.log(a)
+console.log(b)
