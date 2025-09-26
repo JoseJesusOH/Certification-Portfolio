3dff128726593f8b91ce048f2a12e2c0aa368de6
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-25 17:25:45 -0700

    Operadores Comparaacion - JSU
    Se realiza la implementación de los comparadores de compación en JS == y ===

---
+++ JavaScript-Universidad/CodigoJS-Curso-P1/operadores-comparacion.js
@@ -0,0 +1,12 @@
+let a=5;
+let b='5';
+console.log(a)
+console.log(b)
+
+console.log("a == b  -> ",a==b)
+//Interpolacion String
+console.log(`${a}==${b} ->  ${a==b}`)
+
+console.log("a === b  -> ",a===b)
+//Interpolacion String
+console.log(`${a}===${b} ->  ${a===b}`)
