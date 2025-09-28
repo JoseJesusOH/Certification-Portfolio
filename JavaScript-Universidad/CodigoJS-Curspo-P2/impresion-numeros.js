e3d11e0853e0d35ad5ada4eb66e419830625c6d5
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-28 14:55:59 -0700

    Impresion Numeros Ciclo For - CEP JSU
    Ejemplo de impresion con ciclo for.

---
+++ JavaScript-Universidad/CodigoJS-Curspo-P2/impresion-numeros.js
@@ -0,0 +1,9 @@
+let maximo=10,minimo=-10;
+console.log("Incrementos de 3 en 3")
+for (let contador=1; contador<=maximo;contador+=3){
+  console.log(contador)
+}
+console.log("Decrementos de 3 en 3")
+for (let contador=1; contador>=minimo;contador-=3){
+  console.log(contador)
+}
