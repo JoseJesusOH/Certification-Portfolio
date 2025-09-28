334dfb744513191b5d17a3fca5af032da60f5033
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-28 15:03:31 -0700

    Acumulador Numeros 2 - CEP JSU
    Version 2 de acumuladores JS.

---
+++ JavaScript-Universidad/CodigoJS-Curspo-P2/acumulador2.js
@@ -0,0 +1,15 @@
+let suma=0;
+let maximo =5;
+do{
+    suma=suma+contador
+    contador++;
+}
+while(contador<=maximo)
+/*
+while(contador<=maximo)
+    {
+    suma=suma+contador
+    contador++;
+}
+    */
+console.log(suma)
