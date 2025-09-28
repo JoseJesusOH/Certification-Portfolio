64a16dc2693ebbf86caf1698e20d2d6b2499ba85
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-28 12:48:32 -0700

    Operadores Orden - CEP JSU
    Ejercicio basico del orden de operadores y ejeución.

---
+++ JavaScript-Universidad/CodigoJS-Curso-P1/operadores-orden.js
@@ -0,0 +1,14 @@
+// Orden
+/*
+ Parentesis,Corchetes
+ Operadores unitarios  - -- ++ !
+ Aritmeticos *  /  %
+ Aritmeticos + - 
+ Relacionales >= <= > <
+ Igualdad == != 
+ Logicos && || 
+ Asignacion =+ =  -= *=  
+ */
+
+ let a=12/3 +2*3-1
+ console.log(a)
