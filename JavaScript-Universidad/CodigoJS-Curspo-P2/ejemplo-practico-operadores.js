1f096c2141ac94e048129bb57b025ca819313ee9
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-28 14:23:40 -0700

    Persona Mayor Edad - CEP JSU
    Ejemplo para determinar persona mayoor de edad.

---
+++ JavaScript-Universidad/CodigoJS-Curspo-P2/ejemplo-practico-operadores.js
@@ -0,0 +1,8 @@
+const EDAD_ADULTO=20;
+let edadPersona=7;
+
+if(edadPersona>=EDAD_ADULTO){
+    console.log(` La persona con edad ${edadPersona} es un adulto`)
+}else{
+    console.log(`La persona con edad ${edadPersona} es menor de edad`)
+}
