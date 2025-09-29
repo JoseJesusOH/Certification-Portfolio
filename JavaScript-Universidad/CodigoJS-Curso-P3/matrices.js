11814fcbb85ee8a585a0320c884369c06ed5406b
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-29 15:32:29 -0700

    Ejemplo Matrices Js - CEP JSU
    Ejemplo de la implementacion de matrices en JS.

---
+++ JavaScript-Universidad/CodigoJS-Curso-P3/matrices.js
@@ -0,0 +1,12 @@
+let matriz= [[],[]]
+
+matriz[0][0]=10
+matriz[0][1]=20
+matriz[0][2]=30
+
+matriz[1][0]=40
+matriz[1][1]=50
+matriz[1][2]=60
+
+console.log(`Elemento [0][1]`,matriz[0][1])
+console.log(`Elemento [1][2]`,matriz[1][2])
