c967256f00c6be89f12b0df372247a56c599bff5
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-09-29 15:46:28 -0700

    Matriz iteracion JS - CEP JSU
    Se realiza iteracion de matriz en JS

---
+++ JavaScript-Universidad/CodigoJS-Curso-P3/matrices-iterar.js
@@ -0,0 +1,8 @@
+let matriz= [[10,20,30],[40,50,60]]
+
+
+for(let renglon=0;renglon<matriz.length;renglon++){
+   for(let columna=0;columna<matriz[renglon].length;columna++){
+   console.log(`Elemento [${renglon}][${columna}] `,matriz[renglon][columna])
+   }
+}
