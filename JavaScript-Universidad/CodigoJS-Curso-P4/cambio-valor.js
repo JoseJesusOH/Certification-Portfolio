3c23cee7263d2f0046c123c69ec642e9b264e312
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-03 17:32:31 -0700

    Ejercicio CambioValor - CEP JSU
     Ejercio JS del curso udemy realizado.

---
+++ JavaScript-Universidad/CodigoJS-Curso-P4/cambio-valor.js
@@ -0,0 +1,8 @@
+function cambiarValor(parametro){
+    parametro="Adios"
+}
+let argumento="Hola"
+argumento="Hola Mundo"
+console.log(`Antes de funcion: ${argumento}`)
+cambiarValor(argumento)
+console.log(`Despues de funcion: ${argumento}`)
