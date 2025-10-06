caa5541de758008a73f514d38667a0ecd758c6d3
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-06 16:10:23 -0700

    Sobrescritura en objetos - CEP JSU
    Sobrescritura en JS.

---
+++ JavaScript-Universidad/CodigoJS-Curso-P7/clases-sobreescritura.js
@@ -0,0 +1,39 @@
+class Persona{
+    constructor(nombre,apellido){
+ this.nombre=nombre;
+ this.apellido=apellido;
+    }
+    get nombre(){
+        return this.nombre;
+    }
+    set nombre(nombre){
+ this.nombre=nombre;
+    }
+    nombreCompleto(){
+      return this.nombre+" "+this.apellido
+    }
+}
+
+class Empleado extends Persona{
+ constructor(nombre,apellido,departamento){
+    super(nombre,apellido)
+this._departamento=departamento
+ }
+ get departamento(){
+    return this._departamento;
+ }
+ set departamento(departamento){
+    this._departamento=departamento
+ }
+ nombreCompleto(){
+   return super.nombreCompleto()+" "+this._departamento
+ }
+}
+
+
+let persona1=new Persona("Juan","Barrera");
+console.log(persona1)
+
+let empleado1=new Persona("Maria","Dominguez","Sistemas")
+console.log(empleado1)
+console.log(empleado1.nombreCompleto())
