817dd2d626227a12bd6c1a93ee79507471eb636b
JoseJesusOH <josejesus.softwareengineer@gmail.com>
2025-10-06 16:04:15 -0700

    Ejemplo de clases en JS - CEP JSU
    Se realiza ejempolo de clases en JS.

---
+++ JavaScript-Universidad/CodigoJS-Curso-P7/clases.js
@@ -0,0 +1,12 @@
+class Persona{
+    constructor(nombre,apellido){
+ this.nombre=nombre;
+ this.apellido=apellido;
+    }
+}
+
+let persona1=new Persona("Juan","Barrera");
+console.log(persona1)
+
+let persona2=new Persona("Jose","Barrera");
+console.log(persona2)
